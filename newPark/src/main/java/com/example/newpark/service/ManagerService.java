fd087753a52aa87eb191f730164444d3c84d0639
hhhu04 <hhhu04@gmail.com>
2021-06-08 18:12:12 +0900

    관리자 기능 일부

--- newPark/src/main/java/com/example/newpark/service/ManagerService.java
+++ newPark/src/main/java/com/example/newpark/service/ManagerService.java
@@ -1,15 +1,23 @@
 package com.example.newpark.service;
 
 import com.example.newpark.domain.Manager;
+import com.example.newpark.domain.Member;
+import com.example.newpark.domain.PaymentLogs;
 import com.example.newpark.repository.ManagerRepositor;
+import com.example.newpark.repository.MemberRepository;
+import com.example.newpark.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ManagerService {
 
     private final ManagerRepositor managerRepositor;
+    private final PaymentRepository paymentRepository;
+    private final MemberRepository memberRepository;
 
 
     public int login(Manager manager) throws Exception{
@@ -20,7 +28,21 @@ public class ManagerService {
     }
 
 
+    public List<Manager> findAll() throws Exception{
+        List<Manager> list;
+        list = managerRepositor.findAll();
+        return list;
+    }
 
+    public List<PaymentLogs> findPayAll() throws Exception{
+        List<PaymentLogs> list;
+        list = paymentRepository.findAll();
+        return list;
+    }
 
-
+    public List<Member> findMemAll() throws Exception{
+        List<Member> list = null;
+        list = memberRepository.findMemberByMemberGrade("member");
+        return list;
+    }
 }
