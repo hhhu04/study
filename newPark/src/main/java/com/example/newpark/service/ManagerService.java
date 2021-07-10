62164c4b67c9889b0ed0033967dc8d23b27e9509
hhhu04 <hhhu04@gmail.com>
2021-07-10 14:55:17 +0900

    .

--- newPark/src/main/java/com/example/newpark/service/ManagerService.java
+++ newPark/src/main/java/com/example/newpark/service/ManagerService.java
@@ -8,7 +8,6 @@ import com.example.newpark.repository.ManagerRepositoy;
 import com.example.newpark.repository.MemberRepository;
 import com.example.newpark.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
